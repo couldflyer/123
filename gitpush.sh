8c62795481bc27d0dae30f80deb4fe8c840f46bc
couldflyer <couldflyer@users.noreply.github.com>
2024-08-14 04:45:57 +0000

    deploy: 6adfe9d0d766a8de7ea0e0b63bd00e493f36146f

--- gitpush.sh
+++ gitpush.sh
@@ -12,13 +12,22 @@ if [ $# -eq 1 ]; then
 fi
 
 # 添加所有更改
-git add .
+if ! git add .; then
+  echo "错误: 添加更改时失败。"
+  exit 1
+fi
 
 # 提交更改
-git commit -m "$commit_message"
+if ! git commit -m "$commit_message"; then
+  echo "错误: 提交更改时失败。"
+  exit 1
+fi
 
 # 推送到默认分支
-git push origin $(git branch --show-current)
+if ! git push origin $(git branch --show-current); then
+  echo "错误: 推送更改时失败。"
+  exit 1
+fi
 
 # 输出操作结果
 echo "更改已推送到仓库，提交信息为: $commit_message"
